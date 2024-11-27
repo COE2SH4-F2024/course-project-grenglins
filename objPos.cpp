919a644ab70c4b761daeb3edf2070011f4e4d183
Zoish Golwala <golwalaz@mcmaster.ca>
2024-11-27 15:31:50 -0500

    Iteration 0

--- objPos.cpp
+++ objPos.cpp
@@ -18,9 +18,39 @@ objPos::objPos(int xPos, int yPos, char sym)
 
 // Respect the rule of six / minimum four
 // [TODO] Implement the missing special member functions to meet the minimum four rule
+objPos :: ~objPos()
+{
+    delete pos;
+}
 
+objPos::objPos(const objPos &other)
+{
+    pos = new Pos; 
 
+    pos->x = other.pos->x;
+    pos->y = other.pos->y;
+    symbol = other.symbol; 
 
+}
+
+objPos &objPos:: operator =(const objPos &other)
+{
+    if (this == &other)
+    {
+        return *this;
+    }
+
+    delete pos;
+
+    pos = new Pos;
+    
+    pos->x = other.pos->x;
+    pos->y = other.pos->y;
+    symbol = other.symbol;
+
+    return *this;
+
+}
 
 void objPos::setObjPos(objPos o)
 {
